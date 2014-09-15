a75bec136d2063d7228d00fc5f85406cca1e112d
janhink <jan.hink@topmonks.com>
2014-09-15 17:13:57 +0200

    Master changed

--- src/com/topmonks/gittest/Main.java
+++ src/com/topmonks/gittest/Main.java
@@ -3,7 +3,7 @@ package com.topmonks.gittest;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
+	    System.out.println("Hello World (changed)!");
         System.out.println("New feature implemented!");
     }
 }
